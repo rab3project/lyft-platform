8e06ba35f7d4fcfd58304be094632384823de5eb
hsakarp <kunwar.prakash.man@gmail.com>
2019-09-12 11:57:47 -0400

    new project added

--- src/main/java/com/rab3/controllers/DriverSignUpController.java
+++ src/main/java/com/rab3/controllers/DriverSignUpController.java
@@ -1,21 +1,38 @@
 package com.rab3.controllers;
 
-import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.rab3.dtos.DriverSignUpDto;
 import com.rab3.services.DriverSignUpService;
 
-@Controller 
-@RequestMapping(value="register")
+@RestController 
+//@RequestMapping(value="/register")
 public class DriverSignUpController {
 
 	private DriverSignUpService driverSignUpService;
 	
 	@RequestMapping(value="driver", method = RequestMethod.POST)
-	public void createDriver(DriverSignUpDto driverSignUpDto) {
+	public void createDriver(@RequestBody DriverSignUpDto driverSignUpDto) {
 		driverSignUpService.DriverRegister(driverSignUpDto);
 	}
+	
+	@RequestMapping(value="/driver", method = RequestMethod.GET)
+	public void test() {
+		System.out.println("This is test");
+	}
+
+	public DriverSignUpService getDriverSignUpService() {
+		return driverSignUpService;
+	}
+
+	public void setDriverSignUpService(DriverSignUpService driverSignUpService) {
+		this.driverSignUpService = driverSignUpService;
+	}
+	
+	
 	
 }
--- src/main/java/com/rab3/dtos/DriverSignUpDto.java
+++ src/main/java/com/rab3/dtos/DriverSignUpDto.java
@@ -6,42 +6,61 @@ public class DriverSignUpDto {
 	private String address;
 	private String phoneNumber;
 	private String email;
+	private String username;
 	private String password;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public String toString() {
-		return "SignUpDto [name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber + ", email=" + email
-				+ ", password=" + password + "]";
+		return "DriverSignUpDto [name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber + ", email="
+				+ email + ", username=" + username + ", password=" + password + "]";
 	}
-	
-	
+
 }
